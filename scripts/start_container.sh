7d373f96f2968632e333f52411f8d81cec8df6c6
saiashmitha16-hub <saiashmitha16@gmail.com>
2026-03-03 20:40:34 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,6 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull saianju/simple-python-flask-app:latest
 
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 --name simple-python-app saianju/simple-python-flask-app:latest
